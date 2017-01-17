0f94f423230881341b6fa1c15b9c2c5e77026e3d
Travis Baker <travis.baker@usaa.com>
2017-01-17 12:19:47 -0600

    Small updates and tweaks to UI

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -16,10 +16,6 @@ class ViewController: UIViewController {
         "Me My Mo M<SHORT_NAME>",
         "<FULL_NAME>"].joined(separator: "\n")
     
-    enum NameValidationError: Error {
-        case noVowels
-    }
-    
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var lyricsView: UITextView!
 
@@ -46,43 +42,35 @@ class ViewController: UIViewController {
             return
         }
         
-        if let lyrics = try? lyricsForName(lyricsTemplate: bananaFanaTemplate, firstName: nameField.text!) {
-            lyricsView.textColor = UIColor.black
-            lyricsView.text = lyrics
-        } else {
-            lyricsView.textColor = UIColor.red
-            lyricsView.text = "The name \(nameField.text!) will not work for Silly Song. The name has to have a vowel (a, e, i, o, u) in order to work."
-        }
+        lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, firstName: nameField.text!)
     }
     
     // MARK: Utility functions
     // Returns a short name that is lowercase with any leading consonants removed
-    func shortNameFromName(name: String) throws -> String {
-        var name = name.lowercased()
+    func shortNameFromName(name: String) -> String {
+        var modifiedName = name.lowercased()
         // Remove any accents from the string
-        var localizedName = String(data: name.data(using: .ascii, allowLossyConversion: true)!, encoding: .ascii)!
+        var localizedName = String(data: modifiedName.data(using: .ascii, allowLossyConversion: true)!, encoding: .ascii)!
         var firstChar = localizedName[localizedName.startIndex]
         // Remove consonants from beginning of string
         while !vowels.contains(firstChar) {
-            name.remove(at: name.startIndex)
+            modifiedName.remove(at: modifiedName.startIndex)
             localizedName.remove(at: localizedName.startIndex)
             // Make sure we don't run into array out of bounds exception
             if localizedName.characters.count == 0 {
-                throw NameValidationError.noVowels
+                return name.lowercased()
             }
             firstChar = localizedName[localizedName.startIndex]
         }
-        return name
+        return modifiedName
     }
     
-    func lyricsForName(lyricsTemplate: String, firstName: String) throws -> String {
+    func lyricsForName(lyricsTemplate: String, firstName: String) -> String {
         let firstName = firstName.capitalized
-        // Will throw error if name validation failed
-        let shortName = try shortNameFromName(name: firstName)
         
         return lyricsTemplate
             .replacingOccurrences(of: "<FULL_NAME>", with: firstName)
-            .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+            .replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(name: firstName))
     }
 
 }
